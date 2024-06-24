bcd0f3a8138d8cd55db256009f12ea35f9619bcb
Mohammad Jami <anssjami@gmail.com>
2024-06-24 23:15:48 +0600

    hello there

---
+++ lib/1st_live_test.dart
@@ -0,0 +1,29 @@
+class Media {
+  void play() {
+    print("Playing media...");
+  }
+}
+
+
+class Song extends Media {
+  String artist;
+
+  Song(this.artist);
+
+  @override
+  void play() {
+    print("Playing song by $artist...");
+  }
+}
+
+
+void main() {
+
+  Media media = Media();
+
+  Song song = Song("Ed Sheeran");
+
+  media.play();
+
+  song.play();
+}
